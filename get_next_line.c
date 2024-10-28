c4214fb7d3c947c4cf4b08496d3ca44ae6a9ecf2
Jorge Gomez De Oliveira <jgomez-d@c3r10s4.42madrid.com>
2024-10-28 17:56:50 +0100

    Last push before home

--- get_next_line.c
+++ get_next_line.c
@@ -6,23 +6,22 @@
 /*   By: jgomez-d <jgomez-d@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 03:10:54 by jgomez-d          #+#    #+#             */
-/*   Updated: 2024/10/28 06:40:30 by jgomez-d         ###   ########.fr       */
+/*   Updated: 2024/10/28 17:52:19 by jgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_line(int fd, t_list *str)
+char	*get_line(int fd, t_list *str, char	*aux)
 {
 	size_t	cont;
-	char	*aux;
-	
 //	SI HAY CONTENIDO GUARDADO EN EL NODO DE LA LISTA
 	if (str->content)
+	{
 //		SI HAY UN SALTO DE LINEA EN LA PRIMERA POSICION (SON RESTOS)
 		if (str->content[0] == '\n')
 		{
-			cont = read(fd, aux, BUFFER_SIZE - ft_strlen(str->content));
+			cont = read(fd, aux, (BUFFER_SIZE - ft_strlen(str->content)));
 			if (cont < 0)
 				return (NULL);
 			str->content = ft_strjoin(str->content, aux);
@@ -32,38 +31,39 @@ char	*get_line(int fd, t_list *str)
 		{
 			str->next = ft_lstnew(aux);
 			if (!str->next)
-				return (NULL);	
+				return (NULL);
 		}
+	}
 //	NO HAY NADA EN EL CONTENIDO DEL NODO
 	else
 	{
 		cont = read(fd, aux, BUFFER_SIZE);
 		if (cont < 0)
 			return (NULL);
-		ft_memcpy(str->content, aux, (aux - ft_strchr(aux, '\n')));
+		ft_memcpy(str->content, aux, ft_strlen(aux)
+			- ft_strlen(ft_strchr(aux, '\n')));
 		str->content = aux;
 		return (ft_strchr(aux, '\n'));
 	}
 	return (NULL);
-	
 }
 
 char	*get_next_line(int fd)
 {
 	static t_list	*str_list;
 	char			*nxt_ln;
+	char			*buff;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	nxt_ln = get_line(str);
+	nxt_ln = get_line(fd, str_list, buff);
 	if (nxt_ln)
 	{
-		while (ft_strchr(nxt_ln, '\n'))
+		while (ft_strchr(&nxt_ln[1], '\n'))
 		{
-			ft_lstadd_back(&str_list, ft_lstnew(ft_strchr(nxt_ln, '\n')));
-			ft_memcpy(nxt_ln, aux, (aux - ft_strchr(aux, '\n')));
+			ft_lstadd_back(&str_list, ft_lstnew(ft_strchr(nxt_ln[1], '\n')));
+			ft_memcpy(nxt_ln, buff, (buff - ft_strchr(buff, '\n')));
 		}
-		
 	}
 	return (str_list->content);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jgomez-d <jgomez-d@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 03:10:47 by jgomez-d          #+#    #+#             */
-/*   Updated: 2024/10/26 14:34:58 by jgomez-d         ###   ########.fr       */
+/*   Updated: 2024/10/28 17:53:52 by jgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@
 # include <stdlib.h>
 
 char	*get_next_line(int fd);
+typedef struct s_list
+{
+	char			content[BUFFER_SIZE + 1];
+	struct s_list	*next;
+}	t_list;
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
-typedef struct s_list
-{
-	char			content[BUFFER_SIZE + 1];
-	struct s_list	*next;
-}	t_list;
 
 #endif
